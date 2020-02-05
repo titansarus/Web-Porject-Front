acaf4583210339da5b6e4276536e3f12ecca7767
titansarus <amirm137878@gmail.com>
2020-02-05 13:44:54 +0330

    Profile Some CHanges

--- src/components/profile/follow.js
+++ src/components/profile/follow.js
@@ -2,17 +2,59 @@ import React, {Component} from 'react'
 import "./style.css"
 
 class Follow extends Component {
+    async insideSubmit() {
+        let thisIS = this;
+
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", "Bearer " + localStorage.getItem("ACCESS_TOKEN"))
+
+        let url = "http://127.0.0.1:8000/api/chanel/follow/";
+        let a = window.location.href
+        let sp= a.split('/')
+        console.log("LENGTH" + sp.length)
+        let profile_name = sp[sp.length-1]
+
+        url = url + profile_name;
+
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+        fetch(url, requestOptions)
+            .then(response => response.text())
+            .then(function (result) {
+                console.log(result)
+                let obj = JSON.parse(result);
+                 msg = obj.msg;
+                console.log("msg" + msg)
+
+
+            })
+            .catch(error => {
+                alert('error' + error)
+            });
+
+
+
+
+    }
+
     render() {
         let button;
-        if(this.props.me.follow)
-            button = <a href="#" class="nav-item nav-link activer" title="unfollow"><i className="	fas fa-user-minus"></i> </a>;
-        else
-            button = <a href="#" class="nav-item nav-link active" title="Follow"><i className="fas fa-user-plus"></i> </a>;
+        // if(this.props.me.follow)
+        //     button = <a href="#" class="nav-item nav-link activer" title="unfollow"><i className="	fas fa-user-minus"></i> </a>;
+        // else
+        button = <a onClick={this.insideSubmit.bind(this)} className="nav-item nav-link active" title="Follow"><i className="fas fa-user-plus"></i> </a>;
         return (
             <div className="navbar-nav">
-                { button }
+                {button}
             </div>
         )
     }
 }
+
 export default Follow
