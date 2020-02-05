8a0192aab836e7c6a8e871679ab7b21613c87408
titansarus <amirm137878@gmail.com>
2020-02-05 14:47:13 +0330

    Some Following

--- src/components/profile/follow.js
+++ src/components/profile/follow.js
@@ -2,6 +2,47 @@ import React, {Component} from 'react'
 import "./style.css"
 
 class Follow extends Component {
+    state = {
+        shouldShowFollow: [],
+
+    }
+
+    componentDidMount() {
+        const that = this;
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", "Bearer " + localStorage.getItem("ACCESS_TOKEN"))
+
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+        let url = "http://127.0.0.1:8000/api/account/";
+
+        url = url + "me";
+
+
+        fetch(url, requestOptions)
+            .then(response => response.text())
+            .then(function (result) {
+
+                //console.log(result)
+                let obj = JSON.parse(result);
+                let user_data = obj.data.user_data;
+                console.log(obj)
+                console.log(that)
+
+
+            })
+            .catch(error => {
+                alert('error' + error)
+                return
+            });
+    }
+
     async insideSubmit() {
         let thisIS = this;
 
@@ -12,9 +53,9 @@ class Follow extends Component {
 
         let url = "http://127.0.0.1:8000/api/chanel/follow/";
         let a = window.location.href
-        let sp= a.split('/')
+        let sp = a.split('/')
         console.log("LENGTH" + sp.length)
-        let profile_name = sp[sp.length-1]
+        let profile_name = sp[sp.length - 1]
 
         url = url + profile_name;
 
@@ -24,21 +65,24 @@ class Follow extends Component {
             headers: myHeaders,
             redirect: 'follow'
         };
+        var obj
+        var msg;
         fetch(url, requestOptions)
             .then(response => response.text())
             .then(function (result) {
                 console.log(result)
-                let obj = JSON.parse(result);
-                let msg = obj.msg;
+                obj = JSON.parse(result);
+                msg = obj.msg;
                 console.log("msg" + msg)
 
 
             })
             .catch(error => {
                 alert('error' + error)
+                return
             });
-
-
+        console.log(msg);
+        console.log(thisIS)
 
 
     }
@@ -48,7 +92,8 @@ class Follow extends Component {
         // if(this.props.me.follow)
         //     button = <a href="#" class="nav-item nav-link activer" title="unfollow"><i className="	fas fa-user-minus"></i> </a>;
         // else
-        button = <a onClick={this.insideSubmit.bind(this)} className="nav-item nav-link active" title="Follow"><i className="fas fa-user-plus"></i> </a>;
+        button = <a onClick={this.insideSubmit.bind(this)} className="nav-item nav-link active" title="Follow"><i
+            className="fas fa-user-plus"></i> </a>;
         return (
             <div className="navbar-nav">
                 {button}
