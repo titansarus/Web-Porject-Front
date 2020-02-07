90896c236d9798497967b874b44bd8e06a89a95c
titansarus <amirm137878@gmail.com>
2020-02-07 13:19:24 +0330

    some channel changes

--- src/components/Channel/channel.js
+++ src/components/Channel/channel.js
@@ -44,7 +44,7 @@ class ChannelPage extends Component {
             .then(response => response.text())
             .then(function (result) {
 
-                console.log(result)
+               console.log(result)
                 let obj = JSON.parse(result);
                 let data = obj.data;
                 let title = data.title;
@@ -61,16 +61,16 @@ class ChannelPage extends Component {
                 }
                 let identifier = data.identifier;
                 let owners = data.owner;
-                let authors = data.author;
+                var authors = data.author;
 
                 let owner_string = owners.username
-                console.log(authors)
+                //console.log(authors)
 
                 let author_str = ""
                 for (let author of authors) {
                     // console.log("AUTHORAR: "+author)
                     author_str = author_str + "<a href=\"http://localhost:3000/profile/other/ " + author.username + "\">" + author.username + "</a> <br/>"
-                    console.log(author_str)
+                    //console.log(author_str)
                 }
 
                 document.getElementById("Authors").innerHTML = author_str;
@@ -82,9 +82,78 @@ class ChannelPage extends Component {
                     identifier: identifier,
                     owner: owner_string,
                     authors: author_str,
-                })
+                });
 
-                console.log(data.author)
+                {
+                    var myHeaders2 = new Headers();
+                    myHeaders2.append("Content-Type", "application/json");
+                    myHeaders2.append("Authorization", "Bearer " + localStorage.getItem("ACCESS_TOKEN"))
+
+                    var requestOptions2 = {
+                        method: 'GET',
+                        headers: myHeaders2,
+                        redirect: 'follow'
+                    };
+
+                    let a2 = window.location.href
+                    let re2 = /^http:\/\/(localhost|127\.0\.0\.1):3000\/channel\/(\w+)\/?$/
+                    let profile_name2 = a2.match(re)[2];
+
+                    let url2 = "http://127.0.0.1:8000/api/posts/chanel/";
+
+                    url2 = url2 + profile_name;
+
+
+                    fetch(url2, requestOptions2)
+                        .then(response2 => {
+                            let response2_json = response2.json();
+                            // console.log(response2_json);
+                            return response2_json
+                        })
+                        .then(function (result2) {
+
+
+                            let output_str = "<br/>"
+                            for (let msg of result2.msg) {
+
+                                let author_id = msg.author;
+                                let author_name
+                                for (let author_ of authors)
+                                {
+                                    if (author_.id = author_id)
+                                    {
+                                        author_name = author_.username;
+                                    }
+                                }
+                                let stringRes = '<div class="row">\n' +
+                                    '    <div class="col">\n' +
+                                    '        <div class="card">\n' +
+                                    '            <div class="card-body">\n' +
+                                    '                <h4 class="card-title">' + msg.title + '</h4>\n' +
+                                    '                <h6 class="text-muted card-subtitle mb-2">'+ author_name + "::: " + msg.create_time + '</h6>\n' +
+                                    '                <p class="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><a class="card-link" href="#"><i class="fa fa-plus"></i><span>Text</span></a>\n' +
+                                    '                <a\n' +
+                                    '                    class="card-link" href="#"><i class="fa fa-minus"></i><span>Text</span></a>\n' +
+                                    '            </div>\n' +
+                                    '        </div>\n' +
+                                    '    </div>\n' +
+                                    '</div>'
+                                console.log(stringRes)
+                            }
+                            //let newObj = JSON.parse(result2);
+
+                            //console.log("newObj:" + newObj.toString());
+
+
+                        })
+                        .catch(error2 => {
+                            alert('error0 ' + error2)
+                            return
+                        });
+                }
+
+
+
 
 
             })
@@ -92,6 +161,10 @@ class ChannelPage extends Component {
                 alert('error0 ' + error)
                 return
             });
+
+
+
+
     }
 
     render() {
