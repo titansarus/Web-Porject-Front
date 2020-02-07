214fef98ad26cb9c8e06ef7d492a3d3ed8d65837
titansarus <amirm137878@gmail.com>
2020-02-07 20:24:44 +0330

    Some Refactorizations

--- src/components/Channel/channel.js
+++ src/components/Channel/channel.js
@@ -1,10 +1,6 @@
 import React, {Component} from 'react';
-import UserCard from "../profile/UserCard";
-import ChannelCard from "../profile/ChannelCard";
-import Users from "./../../static/jsons/profiles"
 import "./topic.css"
 import {Redirect} from "react-router-dom";
-import Swal from 'sweetalert2'
 
 
 class ChannelPage extends Component {
@@ -95,29 +91,159 @@ class ChannelPage extends Component {
             });
     }
 
+    async addEventListeners(msgs, that, amIAuthor) {
+        for (let msg of msgs) {
+            console.log("MSG.id: " + msg.id)
+            document.getElementById("like_" + msg.id).addEventListener("click", function () {
+                that.addLikeDislike(1, msg.id)
+            });
+
+            document.getElementById("dislike_" + msg.id).addEventListener("click", function () {
+                that.addLikeDislike(-1, msg.id)
+            });
+            if (amIAuthor) {
+
+                document.getElementById("delete_post_" + msg.id).addEventListener("click", function () {
+
+                    const Swal = require('sweetalert2')
+                    Swal.fire({
+                        title: 'Are you sure?',
+                        text: "You won't be able to revert this!",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Yes, delete it!'
+                    }).then((result) => {
+                        if (result.value) {
+                            that.deletePost(msg.id).catch((error) => {
+
+                            })
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Post has been deleted.',
+                                'success'
+                            ).then((result) => {
+                                window.location.reload(true);
+                            })
+                        }
+                    })
+                })
+            }
+        }
+    }
+
+    async setChannelInfo(data, that) {
+        let title = data.title;
+        if (!title) {
+            title = "NO TITLE";
+        }
+        let rule = data.law;
+        if (!rule) {
+            rule = "NO RULES"
+        }
+        let description = data.description;
+        if (!description) {
+            description = "NO DESCRIPTION"
+        }
+        let identifier = data.identifier;
+        let owners = data.owner;
+        var authors = data.author;
+
+        let owner_string = owners.username
+        //console.log(authors)
+
+        let author_str = ""
+        for (let author of authors) {
+            // console.log("AUTHORAR: "+author)
+            author_str = author_str + "<a href=\"http://localhost:3000/profile/other/ " + author.username + "\">" + author.username + "</a> <br/>"
+            //console.log(author_str)
+        }
+
+        document.getElementById("Authors").innerHTML = author_str;
+
+        that.setState({
+            title: title,
+            rule: rule,
+            description: description,
+            identifier: identifier,
+            owner: owner_string,
+            authors: author_str,
+        });
+    }
+
+
+    setPostsContent(msgs, authors, that) {
+        let output_str = "<br/>"
+        var amIAuthor = false;
+        for (let msg of msgs) {
+
+            let author_id = msg.author;
+            let author_name
+
+            for (let author_ of authors) {
+                if (author_.id == author_id) {
+                    author_name = author_.username;
+                }
+                if (localStorage.getItem("CURRENT_USER_ID") == author_.id) {
+                    amIAuthor = true
+                }
+
+            }
+            console.log("AM I AUTHOR: " + amIAuthor);
+            let msgBody = msg.body;
+            let stringRes = "";
+            if (!amIAuthor) {
+                stringRes = '<div class="row">\n' +
+                    '    <div class="col">\n' +
+                    '        <div class="card_new">\n' +
+                    '            <div class="card-body">\n' +
+                    '                <h4 class="card-title">' + msg.title + '</h4>\n' +
+                    '                <h6 class="text-muted card-subtitle mb-2">' + author_name + "::: " + msg.create_time + '</h6>\n' +
+                    '                <p class="card-text">' + msgBody + '</p><a class="card-link" id =like_' + msg.id + ' href="' + window.location.href + '"><i class="fa fa-plus"></i><span>' + msg.like.liked + '</span></a>\n' +
+                    '                <a\n' +
+                    '                    class="card-link" href="' + window.location.href + '" id="dislike_' + msg.id + '"><i class="fa fa-minus"></i><span>' + msg.like.disLiked + '</span></a>\n' +
+                    '            </div>\n' +
+                    '        </div>\n' +
+                    '    </div>\n' +
+                    '</div>'
+            } else {
+                stringRes = '<div class="row">\n' +
+                    '    <div class="col">\n' +
+                    '        <div class="card_new">\n' +
+                    '            <div class="card-body">\n' +
+                    '                <h4 class="card-title">' + msg.title + '</h4>\n' +
+                    '                <h6 class="text-muted card-subtitle mb-2">' + author_name + "::: " + msg.create_time + '</h6>\n' +
+                    '                <p class="card-text">' + msgBody + '</p><a class="card-link" id =like_' + msg.id + ' href="' + window.location.href + '"><i class="fa fa-plus"></i><span>' + msg.like.liked + '</span></a>\n' +
+                    '                <a\n' +
+                    '                    class="card-link" href="' + window.location.href + '" id="dislike_' + msg.id + '"><i class="fa fa-minus"></i><span>' + msg.like.disLiked * (-1) + '</span></a><a class="card-link" href="#"><i class="fa fa-edit"></i><span>Edit</span></a><a class="card-link" id = "delete_post_' + msg.id + '"><i class="fa fa-trash"></i><span>Delete Post</span></a></div>\n' +
+                    '        </div>\n' +
+                    '    </div>\n' +
+                    '</div>'
+            }
+            console.log(stringRes)
+            output_str = output_str + stringRes;
+
+
+        }
+        return Array(output_str,amIAuthor)
+    }
+
     componentDidMount() {
-
         const that = this;
-
         var myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
         myHeaders.append("Authorization", "Bearer " + localStorage.getItem("ACCESS_TOKEN"))
-
         var requestOptions = {
             method: 'GET',
             headers: myHeaders,
             redirect: 'follow'
         };
-
         let a = window.location.href
         let re = /^http:\/\/(localhost|127\.0\.0\.1):3000\/channel\/(\w+)\/?/
         let profile_name = a.match(re)[2];
-
         let url = "http://127.0.0.1:8000/api/chanel/get/";
-
         url = url + profile_name;
-
-
         fetch(url, requestOptions)
             .then(response => response.text())
             .then(function (result) {
@@ -125,43 +251,9 @@ class ChannelPage extends Component {
                 console.log(result)
                 let obj = JSON.parse(result);
                 let data = obj.data;
-                let title = data.title;
-                if (!title) {
-                    title = "NO TITLE";
-                }
-                let rule = data.law;
-                if (!rule) {
-                    rule = "NO RULES"
-                }
-                let description = data.description;
-                if (!description) {
-                    description = "NO DESCRIPTION"
-                }
-                let identifier = data.identifier;
-                let owners = data.owner;
-                var authors = data.author;
 
-                let owner_string = owners.username
-                //console.log(authors)
-
-                let author_str = ""
-                for (let author of authors) {
-                    // console.log("AUTHORAR: "+author)
-                    author_str = author_str + "<a href=\"http://localhost:3000/profile/other/ " + author.username + "\">" + author.username + "</a> <br/>"
-                    //console.log(author_str)
-                }
-
-                document.getElementById("Authors").innerHTML = author_str;
-
-                that.setState({
-                    title: title,
-                    rule: rule,
-                    description: description,
-                    identifier: identifier,
-                    owner: owner_string,
-                    authors: author_str,
-                });
-
+                that.setChannelInfo(data, that);
+                const authors = data.author;
                 {
                     var myHeaders2 = new Headers();
                     myHeaders2.append("Content-Type", "application/json");
@@ -172,119 +264,27 @@ class ChannelPage extends Component {
                         headers: myHeaders2,
                         redirect: 'follow'
                     };
-
-                    let a2 = window.location.href
-                    let re2 = /^http:\/\/(localhost|127\.0\.0\.1):3000\/channel\/(\w+)\/?$/
-                    let profile_name2 = a2.match(re)[2];
-
-                    let url2 = "http://127.0.0.1:8000/api/posts/chanel/";
-
+                    let url2 = "http://127.0.0.1:8000/api/posts/chanel/"; //FOR GETTING POSTS
                     url2 = url2 + profile_name;
-
-
                     fetch(url2, requestOptions2)
                         .then(response2 => {
-                            let response2_json = response2.json();
-                            // console.log(response2_json);
-                            return response2_json
+                            return response2.json()
                         })
                         .then(function (result2) {
+                            let res = that.setPostsContent(result2.msg, authors, that)
+                            console.log("RES" + res)
+                            let output_str = res[0];
+                            console.log("OUTPUT STR"+ output_str)
+                            let amIAuthor = res[1];
+                            document.getElementById("post-container").innerHTML = output_str;
 
-
-                            let output_str = "<br/>"
-                            for (let msg of result2.msg) {
-
-                                let author_id = msg.author;
-                                let author_name
-                                let amIAuthor = false;
-                                for (let author_ of authors) {
-                                    if (author_.id = author_id) {
-                                        author_name = author_.username;
-                                    }
-                                    if (localStorage.getItem("CURRENT_USER_ID") == author_.id) {
-                                        amIAuthor = true
-                                    }
-
-                                }
-                                console.log("AM I AUTHOR: " + amIAuthor)
-                                let msgBody = msg.body;
-                                let stringRes = "";
-                                if (!amIAuthor) {
-                                    stringRes = '<div class="row">\n' +
-                                        '    <div class="col">\n' +
-                                        '        <div class="card_new">\n' +
-                                        '            <div class="card-body">\n' +
-                                        '                <h4 class="card-title">' + msg.title + '</h4>\n' +
-                                        '                <h6 class="text-muted card-subtitle mb-2">' + author_name + "::: " + msg.create_time + '</h6>\n' +
-                                        '                <p class="card-text">' + msgBody + '</p><a class="card-link" id =like_' + msg.id + ' href="' + window.location.href + '"><i class="fa fa-plus"></i><span>' + msg.like.liked + '</span></a>\n' +
-                                        '                <a\n' +
-                                        '                    class="card-link" href="' + window.location.href + '" id="dislike_' + msg.id + '"><i class="fa fa-minus"></i><span>' + msg.like.disLiked + '</span></a>\n' +
-                                        '            </div>\n' +
-                                        '        </div>\n' +
-                                        '    </div>\n' +
-                                        '</div>'
-                                } else {
-                                    stringRes = '<div class="row">\n' +
-                                        '    <div class="col">\n' +
-                                        '        <div class="card_new">\n' +
-                                        '            <div class="card-body">\n' +
-                                        '                <h4 class="card-title">' + msg.title + '</h4>\n' +
-                                        '                <h6 class="text-muted card-subtitle mb-2">' + author_name + "::: " + msg.create_time + '</h6>\n' +
-                                        '                <p class="card-text">' + msgBody + '</p><a class="card-link" id =like_' + msg.id + ' href="' + window.location.href + '"><i class="fa fa-plus"></i><span>' + msg.like.liked + '</span></a>\n' +
-                                        '                <a\n' +
-                                        '                    class="card-link" href="' + window.location.href + '" id="dislike_' + msg.id + '"><i class="fa fa-minus"></i><span>' + msg.like.disLiked * (-1) + '</span></a><a class="card-link" href="#"><i class="fa fa-edit"></i><span>Edit</span></a><a class="card-link" id = "delete_post_' + msg.id + '"><i class="fa fa-trash"></i><span>Delete Post</span></a></div>\n' +
-                                        '        </div>\n' +
-                                        '    </div>\n' +
-                                        '</div>'
-                                }
-                                console.log(stringRes)
-                                document.getElementById("post-container").innerHTML = stringRes;
-                                document.getElementById("like_" + msg.id).addEventListener("click", function () {
-                                    that.addLikeDislike(1, msg.id)
-                                });
-
-                                document.getElementById("dislike_" + msg.id).addEventListener("click", function () {
-                                    that.addLikeDislike(-1, msg.id)
-                                });
-
-                                document.getElementById("delete_post_" + msg.id).addEventListener("click", function () {
-
-                                    const Swal = require('sweetalert2')
-                                    Swal.fire({
-                                        title: 'Are you sure?',
-                                        text: "You won't be able to revert this!",
-                                        icon: 'warning',
-                                        showCancelButton: true,
-                                        confirmButtonColor: '#3085d6',
-                                        cancelButtonColor: '#d33',
-                                        confirmButtonText: 'Yes, delete it!'
-                                    }).then((result) => {
-                                        if (result.value) {
-                                            that.deletePost(msg.id).catch((error) => {
-
-                                            })
-                                            Swal.fire(
-                                                'Deleted!',
-                                                'Your Post has been deleted.',
-                                                'success'
-                                            ).then((result) => {
-                                                window.location.reload(true);
-                                            })
-                                        }
-                                    })
-                                })
-                            }
-
-
-                            //let newObj = JSON.parse(result2);
-
-                            //console.log("newObj:" + newObj.toString());
-
+                            setTimeout(function () {
+                                that.addEventListeners(result2.msg, that, amIAuthor)
+                            }, 500)
 
                         })
                         .catch(error2 => {
                             alert('error0 ' + error2)
-                            return
                         });
                 }
 
@@ -292,11 +292,11 @@ class ChannelPage extends Component {
             })
             .catch(error => {
                 alert('error0 ' + error)
-                return
             });
 
 
     }
+
 
     render() {
         if (localStorage.getItem("ACCESS_TOKEN") == null && localStorage.getItem("ACCESS_TOKEN") == undefined) {
