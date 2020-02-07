fd5f66c6d337920be9d25ffe46735e301aee2d84
titansarus <amirm137878@gmail.com>
2020-02-07 03:30:46 +0330

    Some Things Done in Channel

--- src/components/Channel/channel.js
+++ src/components/Channel/channel.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import UserCard from "../profile/UserCard";
 import ChannelCard from "../profile/ChannelCard";
 import Users from "./../../static/jsons/profiles"
+import "./topic.css"
 import {Redirect} from "react-router-dom";
 
 
@@ -47,20 +48,40 @@ class ChannelPage extends Component {
                 let obj = JSON.parse(result);
                 let data = obj.data;
                 let title = data.title;
+                if (!title) {
+                    title = "NO TITLE";
+                }
                 let rule = data.law;
+                if (!rule) {
+                    rule = "NO RULES"
+                }
                 let description = data.description;
+                if (!description) {
+                    description = "NO DESCRIPTION"
+                }
                 let identifier = data.identifier;
-                let owner = data.owner;
+                let owners = data.owner;
                 let authors = data.author;
 
+                let owner_string = owners.username
+                console.log(authors)
+
+                let author_str = ""
+                for (let author of authors) {
+                    // console.log("AUTHORAR: "+author)
+                    author_str = author_str + "<a href=\"http://localhost:3000/profile/other/ " + author.username + "\">" + author.username + "</a> <br/>"
+                    console.log(author_str)
+                }
+
+                document.getElementById("Authors").innerHTML = author_str;
 
                 that.setState({
                     title: title,
                     rule: rule,
                     description: description,
                     identifier: identifier,
-                    owner: owner,
-                    authors: authors,
+                    owner: owner_string,
+                    authors: author_str,
                 })
 
                 console.log(data.author)
@@ -80,32 +101,51 @@ class ChannelPage extends Component {
         return (
 
             <div className="container">
-                <h2>{this.state.title + "  " + this.state.identifier}</h2>
-                <br/>
-                <h3>Rules</h3>
-                <hr/>
-                <h4>
-                    {this.state.rule}
-                </h4>
+                <div className="row text-center">
+                    <div className="col channel-titr-container"
+                         style={{backgroundColor: 'rgb(119,255,180)', minHeight: '400px'}}>
+                        Title:
+                        <br/>
+                        <div className="row channel-titr" id="channel-Title"> {this.state.title}</div>
 
-                <h3>Description</h3>
-                <hr/>
-                <h4>
-                    {this.state.description}
-                </h4>
-                <br/>
-                <br/>
-                <br/>
-                <h3>Admin</h3>
-                <hr/>
-                <UserCard user={this.state.owner}/>
-                <br/>
-                <h3>Authors</h3>
-                <hr/>
-                {/*{this.state.authors.map((author) =>*/}
-                {/*    <UserCard user={author} />*/}
-                {/*)}*/}
-                <div id="posts"></div>
+                        <hr/>
+                        Identifier:
+                        <br/>
+                        <div className="row channel-titr"
+                             id="channel-identifier">{this.state.identifier}</div>
+                        <hr/>
+                        Description:
+                        <br/>
+                        <div className="row channel-titr"
+                             id="Channel-description">{this.state.description}</div>
+                        <hr/>
+                        Rules
+                        <br/>
+                        <div className="row channel-titr" id="Channel-Rules">{this.state.rule}</div>
+                        <hr/>
+                        Admin
+                        <br/>
+                        <div className="row channel-titr" id="Admins"> {<a
+                            href={"http://localhost:3000/profile/other/" + this.state.owner}>  {"    " + this.state.owner}</a>}</div>
+                        <hr/>
+                        Authors<br/>
+                        <div className="row channel-titr" id="Authors">
+
+
+                        </div>
+                    </div>
+                    <div className="col-8" id="post-container">
+                        <div className="row"></div>
+                        <div className="row"></div>
+                        <div className="row"></div>
+                        <div className="row"></div>
+                        <div className="row"></div>
+
+                        <div className="row"></div>
+                    </div>
+                </div>
+                <script src="assets/js/jquery.min.js"></script>
+                <script src="assets/bootstrap/js/bootstrap.min.js"></script>
             </div>
         )
     }
