807f2b400a5242c6687181d6e82021b111b9fbd9
mfary <moha1379>
2020-02-10 06:48:40 +0330

    handle search but need query

--- src/template_compnent/Navbar.js
+++ src/template_compnent/Navbar.js
@@ -66,9 +66,9 @@ class Navbar extends Component {
                     </ul>
 
 
-                    <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button className="btn btn-outline-success my-2 my-sm-0" onClick={this.search} type="submit" id="search">Search</button>
+                    <form className="form-inline my-2 my-lg-0" action="http://127.0.0.1:3000/search/">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search"/>
+                        <button className="btn btn-outline-success my-2 my-sm-0"  type="submit" id="search">Search</button>
                     </form>
                     {(!(localStorage.getItem("ACCESS_TOKEN") != null && localStorage.getItem("ACCESS_TOKEN") != undefined)) ? (
                             <ul className="form-inline my-2 my-lg-0 navbar-nav" style={{margin: "10px"}}>
